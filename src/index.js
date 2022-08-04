5c247510a98ae7687579c0677a6cd227e72c92d4
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-08-04 23:14:56 +0200

    Created 2 components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM  from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting()
+ {
+  return (
+    <div>
+      <Person />
+      <Message />
+      
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person = () => <h2>Mukinza</h2>
+const Message = () => {
+  return <p>this is my small project</p>
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('root'))
